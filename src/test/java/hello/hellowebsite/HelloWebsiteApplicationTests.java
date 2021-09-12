47b08a7c29ad63b4e11ea1503573326369855723
이경식 <kyong@kyong.local>
2021-09-12 14:29:08 +0900

    Initial commit

---
+++ src/test/java/hello/hellowebsite/HelloWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
